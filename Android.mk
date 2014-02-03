c304a2265f539d7f19c9f13134c5a048ec9d2ae7
Evisceration <eviscerationls@gmail.com>
2014-02-03 21:15:02 +0100

    Trebuchet: Debrand to CustomLauncher3
    Change-Id: I33857fd6f8907bc7a7ede53fbcaf2026c2d07f74

--- Android.mk
+++ Android.mk
@@ -38,11 +38,11 @@ LOCAL_PROTOC_FLAGS := --proto_path=$(LOCAL_PATH)/protos/
 
 # LOCAL_SDK_VERSION := 21
 
-LOCAL_PACKAGE_NAME := Trebuchet
+LOCAL_PACKAGE_NAME := CustomLauncher3
 LOCAL_PRIVILEGED_MODULE := true
-#LOCAL_CERTIFICATE := shared
+LOCAL_CERTIFICATE := platform
 
-LOCAL_AAPT_FLAGS += --rename-manifest-package com.cyanogenmod.trebuchet
+LOCAL_AAPT_FLAGS += --rename-manifest-package org.namelessrom.customlauncher3
 
 LOCAL_OVERRIDES_PACKAGES := Launcher3
 
--- src/com/android/launcher3/LauncherProvider.java
+++ src/com/android/launcher3/LauncherProvider.java
@@ -868,12 +868,12 @@ public class LauncherProvider extends ContentProvider {
                     db.endTransaction();
                 }
             }
-            // This was the old L2-based Trebuchet's version. Do steps that come after version 12 
+            // This was the old L2-based CustomLauncher3's version. Do steps that come after version 12
             // (Launcher2's original version) so the new things get added, but skip the intermediate
             // workspaceScreens updates (addWorkspacesTable() takes care of that)
 
             if (version == 16) {
-                Log.w(TAG, "Found pre-11 Trebuchet, preparing update");
+                Log.w(TAG, "Found pre-11 CustomLauncher3, preparing update");
 
                 // With the new shrink-wrapped and re-orderable workspaces, it makes sense
                 // to persist workspace screens and their relative order.
